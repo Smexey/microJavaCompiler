ad838e7a71df8578735b17fd4f3d86e6c3798ca5
pajser7256 <pajser7256@gmail.com>
2020-12-25 18:47:04 +0100

    added test

--- src/compiler/ParserTest.java
+++ src/compiler/ParserTest.java
@@ -2,7 +2,6 @@ package compiler;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 
 import org.apache.log4j.Logger;
@@ -22,12 +21,8 @@ public class ParserTest {
 
     public static void main(String[] args) throws Exception {
         Logger log = Logger.getLogger(ParserTest.class);
-        if (args.length < 2) {
-            log.error("Not enough arguments supplied! Usage: MJParser <source-file> <obj-file> ");
-            return;
-        }
 
-        File sourceCode = new File(args[0]);
+        File sourceCode = new File("testFiles/program.mj");
         if (!sourceCode.exists()) {
             log.error("Source file [" + sourceCode.getAbsolutePath() + "] not found!");
             return;
@@ -41,29 +36,10 @@ public class ParserTest {
             Symbol s = p.parse(); // pocetak parsiranja
             SyntaxNode prog = (SyntaxNode) (s.value);
 
-            Tab.init(); // Universe scope
-            SemanticPass semanticCheck = new SemanticPass();
-            prog.traverseBottomUp(semanticCheck);
+            log.info("===========================");
+            log.info(prog.toString());
+            log.info("===========================");
 
-            log.info("Print calls = " + semanticCheck.printCallCount);
-            Tab.dump();
-
-            if (!p.errorDetected && semanticCheck.passed()) {
-                File objFile = new File(args[1]);
-                log.info("Generating bytecode file: " + objFile.getAbsolutePath());
-                if (objFile.exists())
-                    objFile.delete();
-
-                // Code generation...
-                CodeGenerator codeGenerator = new CodeGenerator();
-                prog.traverseBottomUp(codeGenerator);
-                Code.dataSize = semanticCheck.nVars;
-                Code.mainPc = codeGenerator.getMainPc();
-                Code.write(new FileOutputStream(objFile));
-                log.info("Parsiranje uspesno zavrseno!");
-            } else {
-                log.error("Parsiranje NIJE uspesno zavrseno!");
-            }
         }
     }
 }
--- src/compiler/RuleVisitor.java
+++ src/compiler/RuleVisitor.java
@@ -1,7 +1,12 @@
 package compiler;
 
+import ast.MinusExists;
 import ast.VisitorAdaptor;
 
 public class RuleVisitor extends VisitorAdaptor {
+    int minuscnt = 0;
 
+    @Override
+    public void visit(MinusExists minusExists) {
+    }
 }
