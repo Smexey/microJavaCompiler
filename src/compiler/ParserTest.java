86d869b3098ba014dac5cdbddbf0f4562584e5c0
pajser7256 <pajser7256@gmail.com>
2020-12-27 20:55:08 +0100

    change task opeanMainLog

--- src/compiler/ParserTest.java
+++ src/compiler/ParserTest.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
 import ast.Program;
-import ast.SyntaxNode;
 import java_cup.runtime.Symbol;
 
 import util.Log4JUtils;
